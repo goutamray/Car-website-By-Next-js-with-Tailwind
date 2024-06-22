c02f5bf8e3f9dff9671d689906164a4f0551069d
goutam <goutamr362@gmail.com>
2024-06-22 08:21:35 +0600

    all package install & basic setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,43 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//     container : {
+//       padding : {
+//         DEFAULT :  '1rem'
+//       }
+//     }, 
+//     screens: {
+//       sm: '648px',
+//       md: '768px',
+//       lg: '1024px',
+//       xl: '1300px',
+//     },
+//     colors: {
+//       primary : '#101828',
+//       secondary : '#667085', 
+//       accent : {
+//         DEFAULT : '#ed1d24',
+//         hover : '#dd242a',
+//       },
+//       body : '#dedede',
+//     },
+
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,7 +52,29 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+        },
+      },
+      screens: {
+        sm: '648px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1300px',
+      },
+      colors: {
+        primary: '#101828',
+        secondary: '#667085',
+        accent: {
+          DEFAULT: '#ed1d24',
+          hover: '#dd242a',
+        },
+        body: '#dedede',
+      }
+    }
   },
   plugins: [],
 };
+
+
